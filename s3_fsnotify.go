c42c5129f72f440116c1885051b25d0cc101e750
Andy Motta <your@email.com>
2017-02-16 12:34:40 -0800

    Add SSE

--- s3_fsnotify.go
+++ s3_fsnotify.go
@@ -19,7 +19,13 @@ func main() {
 	defer watcher.Close()
 
 	bucket := os.Getenv("bucket")
+	if bucket == "" {
+		log.Fatalln("bucket env var must be set")
+	}
 	filename := os.Getenv("filename")
+	if filename == "" {
+		log.Fatalln("filename env var must be set")
+	}
 
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
@@ -35,22 +41,23 @@ func main() {
 					log.Println("modified file:", event.Name)
 					file, err := os.Open(filename)
 					if err != nil {
-						log.Fatalln("Unable to open file %q, %v", err)
+						log.Fatalln("Unable to open file", err)
 					}
 					defer file.Close()
 
 					uploader := s3manager.NewUploader(sess)
 
 					_, err = uploader.Upload(&s3manager.UploadInput{
-						Bucket: aws.String(bucket),
-						Key:    aws.String(filename),
-						Body:   file,
+						Bucket:               aws.String(bucket),
+						Key:                  aws.String(filename),
+						Body:                 file,
+						ServerSideEncryption: aws.String("AES256"),
 					})
 					if err != nil {
 						// Print the error and exit.
-						log.Fatalln("Unable to upload %q to %q, %v", filename, bucket, err)
+						log.Fatalln("Unable to upload", filename, bucket, err)
 					}
-					fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
+					fmt.Printf("Successfully uploaded", filename, bucket)
 				}
 			case err := <-watcher.Errors:
 				log.Println("error:", err)
